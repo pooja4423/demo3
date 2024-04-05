8d1944b7f8e851e762b56d63c7c76cac7b187151
pooja4423 <164667616+pooja4423@users.noreply.github.com>
2024-04-05 10:31:27 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -4,10 +4,6 @@ num2=20
 
 sum=$(($num1 + $num2))
 sub=$(($num1 - $num2))
-prod=$(($num1 \* $num2))
-div=(($num1 / $num2))
 
 echo "sum of $num1 and $num2 is $sum"
 echo "sub of $num1 and $num2 is $sub"
-echo "prod of $num1 and $num2 is $prod"
-echo "div of $num1 and $num2 is $div"
